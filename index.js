8bec0b0461c889e9c5e7af93b31a40a980093ecb
Realwill10 <gil.william10@gmail.com>
2024-06-06 16:02:36 -0400

    Update index.js
    Error with score variable

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-let homeScoreeEl = document.querySelector("#homeScore");
+let homeScoreEl = document.querySelector("#homeScore");
 let awayScoreEl = document.querySelector("#awayScore");
 let homeScorePts = 0;
 let awayScorePts = 0;
 let scoreOneEl = document.querySelector(".score1");
 let scoreTwoEl = document.querySelector(".score2");
 
-function restart(){
+function restart() {
     homeScorePts = 0;
     awayScorePts = 0;
-    homeScoreeEl.textContent = 0;
+    homeScoreEl.textContent = 0;
     awayScoreEl.textContent = 0;
 }
 
-function increaseOne(){
+function increaseOne() {
     homeScorePts++;
-    homeScoreeEl.textContent = homeScorePts;
+    homeScoreEl.textContent = homeScorePts;
 }
 
-function increaseTwo(){
+function increaseTwo() {
     homeScorePts += 2;
-    homeScoreeEl.textContent = homeScorePts;
+    homeScoreEl.textContent = homeScorePts;
 }
 
-function increaseThree(){
+function increaseThree() {
     homeScorePts += 3;
-    homeScoreeEl.textContent = homeScorePts;
+    homeScoreEl.textContent = homeScorePts;
 }
 
-function increaseAwayOne(){
+function increaseAwayOne() {
     awayScorePts++;
     awayScoreEl.textContent = awayScorePts;
 }
 
-function increaseAwayTwo(){
+function increaseAwayTwo() {
     awayScorePts += 2;
     awayScoreEl.textContent = awayScorePts;
 }
 
-function increaseAwayThree(){
-    awayScorePts +=3;
+function increaseAwayThree() {
+    awayScorePts += 3;
     awayScoreEl.textContent = awayScorePts;
 }
 
-function highlightLeader (){
-    if(homeScorePts > awayScorePts){
+function highlightLeader() {
+    if (homeScorePts > awayScorePts) {
         scoreTwoEl.classList.remove("leaderHighlight");
         scoreOneEl.classList.add("leaderHighlight");
-    } else if (homeScorePts === awayScorePts){
+    } else if (homeScorePts === awayScorePts) {
         scoreTwoEl.classList.remove("leaderHighlight");
         scoreOneEl.classList.remove("leaderHighlight");
     }
-    
+
     else {
         scoreOneEl.classList.remove("leaderHighlight");
         scoreTwoEl.classList.add("leaderHighlight")
