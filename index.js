ab349b6cea3274bb490f5b9505ebdaaa026786ee
Realwill10 <gil.william10@gmail.com>
2024-06-06 15:47:22 -0400

    Commit inital files

---
+++ index.js
@@ -0,0 +1,59 @@
+let homeScoreeEl = document.querySelector("#homeScore");
+let awayScoreEl = document.querySelector("#awayScore");
+let homeScorePts = 0;
+let awayScorePts = 0;
+let scoreOneEl = document.querySelector(".score1");
+let scoreTwoEl = document.querySelector(".score2");
+
+function restart(){
+    homeScorePts = 0;
+    awayScorePts = 0;
+    homeScoreeEl.textContent = 0;
+    awayScoreEl.textContent = 0;
+}
+
+function increaseOne(){
+    homeScorePts++;
+    homeScoreeEl.textContent = homeScorePts;
+}
+
+function increaseTwo(){
+    homeScorePts += 2;
+    homeScoreeEl.textContent = homeScorePts;
+}
+
+function increaseThree(){
+    homeScorePts += 3;
+    homeScoreeEl.textContent = homeScorePts;
+}
+
+function increaseAwayOne(){
+    awayScorePts++;
+    awayScoreEl.textContent = awayScorePts;
+}
+
+function increaseAwayTwo(){
+    awayScorePts += 2;
+    awayScoreEl.textContent = awayScorePts;
+}
+
+function increaseAwayThree(){
+    awayScorePts +=3;
+    awayScoreEl.textContent = awayScorePts;
+}
+
+function highlightLeader (){
+    if(homeScorePts > awayScorePts){
+        scoreTwoEl.classList.remove("leaderHighlight");
+        scoreOneEl.classList.add("leaderHighlight");
+    } else if (homeScorePts === awayScorePts){
+        scoreTwoEl.classList.remove("leaderHighlight");
+        scoreOneEl.classList.remove("leaderHighlight");
+    }
+    
+    else {
+        scoreOneEl.classList.remove("leaderHighlight");
+        scoreTwoEl.classList.add("leaderHighlight")
+
+    }
+}
